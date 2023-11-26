b7b9f73aac4de66eac30821e7473b6b619b2455c
Sharpeter <76182276+CapShift@users.noreply.github.com>
2023-11-26 17:59:33 +0800

    Update qpnp-fg-gen4.c

--- drivers/power/supply/qcom/qpnp-fg-gen4.c
+++ drivers/power/supply/qcom/qpnp-fg-gen4.c
@@ -759,13 +759,10 @@ static int fg_gen4_get_learned_capacity(void *data, int64_t *learned_cap_uah)
 		return rc;
 	}
 
-	/*if (chip->fg_nvmem)
+	if (chip->fg_nvmem)
 		*learned_cap_uah = (buf[0] | buf[1] << 8) * 1000;
 	else
 		*learned_cap_uah = act_cap_mah * 1000;
-  	*/
-
-	*learned_cap_uah = 5160000;
 
 	fg_dbg(fg, FG_CAP_LEARN, "learned_cap_uah:%lld\n", *learned_cap_uah);
 	return 0;
